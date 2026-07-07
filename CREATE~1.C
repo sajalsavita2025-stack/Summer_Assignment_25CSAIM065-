7d57d048a7d962364a9d3d3efbdd7c9f758c0a7c
Sajal savita <sajal.savita2025@glbajajgroup.org>
2026-07-08 02:17:10 +0530

    Add files via upload

---
+++ CREATE~1.C
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+struct Student
+{
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main()
+{
+    struct Student s[100];
+    int n, i;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        printf("\nEnter details of Student %d\n", i + 1);
+
+        printf("Roll No: ");
+        scanf("%d", &s[i].roll);
+
+        printf("Name: ");
+        scanf(" %[^\n]", s[i].name);
+
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+
+    printf("\n----- Student Records -----\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("\nRoll No : %d", s[i].roll);
+        printf("\nName    : %s", s[i].name);
+        printf("\nMarks   : %.2f\n", s[i].marks);
+    }
+
+    return 0;
+}
