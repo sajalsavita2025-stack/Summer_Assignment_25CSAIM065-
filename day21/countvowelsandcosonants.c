46b64512f2aa69be56db742e1acf7e779e3889f3
Sajal savita <sajal.savita2025@glbajajgroup.org>
2026-07-08 00:35:35 +0530

    Add files via upload

---
+++ day21/countvowelsandcosonants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int a[n][m];
+    for(int i=0;i<n;i++) for(int j=0;j<m;j++) scanf("%d",&a[i][j]);
+    int max=a[0][0];
+    for(int i=0;i<n;i++) for(int j=0;j<m;j++) if(a[i][j]>max) max=a[i][j];
+    printf("%d",max);
+    return 0;
+}
