64d2cb3202310f3ea9f9fcb8eea54f65ab51b255
Sajal savita <sajal.savita2025@glbajajgroup.org>
2026-07-08 00:26:12 +0530

    Add files via upload

---
+++ day19/finddiagnol.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n][n];
+    for(int i=0;i<n;i++) for(int j=0;j<n;j++) scanf("%d",&a[i][j]);
+    int sum=0;
+    for(int i=0;i<n;i++) sum+=a[i][n-i-1];
+    printf("%d",sum);
+    return 0;
+}
